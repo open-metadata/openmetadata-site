dfc78f28e45d80d93d1b570630fec4c2422606ad
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-03-23 20:30:29 +0530

    chore: update testimonials details

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,32 +1,33 @@
-const testimonialContainer = document.querySelector("#testimonialContainer");
+const testimonialContainer = document.querySelector('#testimonialContainer');
 
 const fetchTestimonials = async () => {
-  const res = await fetch("./js/testimonials.json");
+  const res = await fetch('./js/testimonials.json');
   const data = await res.json();
 
   testimonialContainer.innerHTML = data
-    .map(({ name, image, description, company }, index) => {
+    .map(({ name, image, description, company, url, title }, index) => {
       return `<div class="swiper-slide" key=${index} >
                   <div class="testimonial-card card card-team">
                     <div class="card-body">
                         <img class="card-team-img mb-3"
                         src=${image}
                         alt=${company}/>
-                      <div class="card-team-name mb-3"><strong>${company}</strong></div>
+                      <div class="card-team-name mb-3"><strong><a href=${url}>${company}</a></strong></div>
                       <p class="mb-0 card-description">
                       ${description}
                       </p>
-                      <p class="card-name"><strong>- ${name}</strong></p>
+                      <p class="card-footer-custom font-500"><strong>- ${name}</strong></p>
+                      <p class="card-footer-custom opacity-6 text-italic">${title}</p>
                     </div>  
                  </div>
               </div>`;
     })
-    .join("");
+    .join('');
 };
 
 fetchTestimonials();
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
   grabCursor: true,
   spaceBetween: 10,
   autoplay: {
@@ -34,8 +35,8 @@ var swiper = new Swiper(".mySwiper", {
   },
   loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   // pagination: {
   //   el: '.swiper-pagination',
