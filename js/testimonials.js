893d4f7778c0cc62ce03b95954b323251ca89393
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-05-03 13:20:12 +0530

    add ZenBusiness testimonial

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,24 +1,4 @@
 const testimonialContainer = document.querySelector('#testimonialContainer');
-const MAX_TESTIMONIAL_WORDS = 60;
-
-const readMoreDescription = (btn) =>{
-  let testimonial = btn.parentElement;
-  testimonial.querySelector(".dots").classList.toggle("hide");
-  testimonial.querySelector(".more").classList.toggle("hide");
-  btn.textContent === "Read more" ? btn.textContent = "Read less" : btn.textContent = "Read more";
-}
-
-const getDescription = (text) => {
-  const textArr = text.split(' ');
-  const showDots = textArr.length > MAX_TESTIMONIAL_WORDS
-  return `<div>
-    <span class="mb-0 card-description main">${textArr.slice(0,MAX_TESTIMONIAL_WORDS).join(' ')}</span>
-    ${showDots ? `<span class="dots">...</span>` : ''}
-    <span class="mb-0 card-description hide more">${textArr.slice(MAX_TESTIMONIAL_WORDS).join(' ')}</span>
-  ${showDots ? `<span class="read-more-button" onclick="readMoreDescription(this)">Read more</span>` : ''}  
-  </div>`
-}
-
 
 const fetchTestimonials = async () => {
   const res = await fetch('./js/testimonials.json');
@@ -33,7 +13,7 @@ const fetchTestimonials = async () => {
                         src=${image}
                         alt=${company}/>
                       <div class="card-team-name mb-3"><strong><a href=${url} target="_blank">${company}</a></strong></div>
-                      ${getDescription(description)}           
+                      ${description}           
                       <p class="card-footer-custom font-500"><strong>- ${name}</strong></p>
                       <p class="card-footer-custom opacity-6 text-italic">${title}</p>
                     </div>  
@@ -58,15 +38,13 @@ var swiper = new Swiper('.mySwiper', {
   breakpoints: {
     640: {
       slidesPerView: 1,
-      spaceBetween: 20,
     },
     768: {
       slidesPerView: 1,
-      spaceBetween: 40,
     },
     1024: {
-      slidesPerView: 2,
-      spaceBetween: 50,
+      slidesPerView: 1,
     },
   },
+  loop: true,
 });
