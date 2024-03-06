f57d69e1f07a221f9c3f36345ba17afe4cfa82ce
Harsh Vador <harsh.vador@somaiya.edu>
2024-03-06 18:55:27 +0530

    fix read all blogs button

--- src/components/Blogs/Blogs.component.tsx
+++ src/components/Blogs/Blogs.component.tsx
@@ -4,9 +4,9 @@ import BlogCard from "../common/BlogCard";
 
 const Blogs = () => {
   return (
-    <section className="bg-gray-100 py-10" id="blogs">
+    <section className="bg-gray-100 py-12" id="blogs">
       <div className="container px-5">
-        <div className="text-center mb-10">
+        <div className="text-center mb-4">
           <h2 className="text-3xl font-semibold">Blogs</h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -20,26 +20,25 @@ const Blogs = () => {
             />
           ))}
         </div>
-        <div className="text-center mt-8">
+        <div className="text-center">
           <a
-            className="inline-block px-6 py-3 text-lg font-semibold rounded-full transition duration-300 ease-in-out bg-blue-600 hover:bg-blue-700 text-white"
+            className="btn btn-primary fw-500"
             href="http://blog.open-metadata.org/"
             target="_blank"
           >
             Read All Blogs
           </a>
         </div>
-      </div>
-      <div className="absolute bottom-0 left-0 w-full text-gray-700">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 144.54 17.34"
-          preserveAspectRatio="none"
-          fill="currentColor"
-          className="block"
-        >
-          <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" />
-        </svg>
+        <div className="svg-border-rounded text-dark">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 144.54 17.34"
+            preserveAspectRatio="none"
+            fill="currentColor"
+          >
+            <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" />
+          </svg>
+        </div>
       </div>
     </section>
   );
