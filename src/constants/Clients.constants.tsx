9b18ce9bb6fa179cb8dc475d645e4b116baf8261
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2024-06-21 16:15:21 +0530

    Add Header Component (#129)
    * add client logos
    
    * add assets for header
    
    * add client component
    
    * add header component
    
    * add header in development page
    
    * add clients list
    
    * add styles for slider images
    
    * reduced the opacity of the client logo
    
    * update css

---
+++ src/constants/Clients.constants.tsx
@@ -0,0 +1,44 @@
+const LOGO_ROUTE = '/assets/clients'
+
+export const CLIENT_LIST = [
+    {
+        name: "inDrive",
+        logo: `${LOGO_ROUTE}/inDrive.svg`,
+        width: 130
+    },
+    {
+        name: "Intelligaia",
+        logo: `${LOGO_ROUTE}/intelligaia.svg`,
+        width: 130
+    },
+    {
+        name: "Beeline",
+        logo: `${LOGO_ROUTE}/beeline.svg`,
+        width: 40
+    },
+    {
+        name: "Khatabook",
+        logo: `${LOGO_ROUTE}/khatabook.svg`,
+        width: 130
+    },
+    {
+        name: "Lahaus",
+        logo: `${LOGO_ROUTE}/lahaus.svg`,
+        width: 130
+    },
+    {
+        name: "Bucketplace",
+        logo: `${LOGO_ROUTE}/bucketplace.svg`,
+        width: 40
+    },
+    {
+        name: "Loggi",
+        logo: `${LOGO_ROUTE}/loggi.svg`,
+        width: 130
+    },
+    {
+        name: "Moove",
+        logo: `${LOGO_ROUTE}/moove.svg`,
+        width: 130
+    },
+]
