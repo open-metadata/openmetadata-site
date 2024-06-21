b9f2dda3cd28b7599a20aa1ff093eab8e0491cb1
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-06-21 18:06:41 +0530

    add list of achievements

---
+++ src/constants/Achievement.constants.tsx
@@ -0,0 +1,24 @@
+const ICON_ROUTE = '/assets/achievement'
+
+export const ACHIEVEMENT_LIST = [
+    {
+        icon: `${ICON_ROUTE}/deployment.svg`,
+        count: '1,000',
+        name: 'Enterprise Deployments'
+    },
+    {
+        icon: `${ICON_ROUTE}/open-source-members.svg`,
+        count: '5,600',
+        name: 'Open Source Members'
+    },
+    {
+        icon: `${ICON_ROUTE}/code-contributors.svg`,
+        count: '250',
+        name: 'Code Contributors'
+    },
+    {
+        icon: `${ICON_ROUTE}/github-star.svg`,
+        count: '4,300',
+        name: 'GitHub Stars'
+    },
+]
