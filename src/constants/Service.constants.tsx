3b69fe23f218d5a437d81a25344538d134e8e30f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-06-24 15:16:48 +0530

    add list of services

---
+++ src/constants/Service.constants.tsx
@@ -0,0 +1,30 @@
+export const SERVICE_LIST = [
+  {
+    serviceName: {
+        header1: 'The only ',
+        header2: 'unified platform',
+        header3: ' for all your use cases'
+    },
+    description:
+      "Get complete data context unified across data discovery, observability, and governance, made possible with a Unified Metadata Graph that centralizes all metadata for all data assets. 80+ turnkey connectors make it easy to collect all your metadata and power a unified experience designed to bring data teams together in shared responsibility and contribution.",
+    icon: "/assets/service/unified-platform.webp",
+  },
+  {
+    serviceName: {
+        header1: 'Built from the ground up for ',
+        header2: 'modern data teams'
+    },
+    description:
+      "OpenMetadata's API and schema-first architecture provides you metadata control and customization. Extensible metadata entities and relationships tailor the Unified Metadata Graph to your specific data needs. This enables data collaboration workflows between data producers and consumers for a more data-driven culture at your organization.",
+    icon: "/assets/service/modern-data-teams.webp",
+  },
+  {
+    serviceName: {
+        header1: 'Designed for ',
+        header2: 'simple setup, operations, and usage'
+    },
+    description:
+      "The streamlined architecture has only four system components, making it easier to deploy, operate, and upgrade compared to other solutions. The simple user experience is designed for technical and non-technical users to collaborate together, while still having robust data practitioner capabilities, all built to meet the security requirements of enterprises.",
+    icon: "/assets/service/simple-setup.webp",
+  },
+];
