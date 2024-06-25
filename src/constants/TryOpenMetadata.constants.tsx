cc405ddb3c25e9a64d819a7a1b6f7ffa984f970d
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-06-25 15:46:20 +0530

    add try openmetadata component

---
+++ src/constants/TryOpenMetadata.constants.tsx
@@ -0,0 +1,25 @@
+const ICON_ROUTE = "/assets/tryOpenMetadata";
+
+export const TRY_OPENMETADATA = [
+  {
+    icon: `${ICON_ROUTE}/deploy.svg`,
+    header: "Deploy",
+    description: "Deploy OpenMetadata in your own environment",
+    linkText: "Download Now",
+    href: "https://docs.open-metadata.org/deployment",
+  },
+  {
+    icon: `${ICON_ROUTE}/experience.svg`,
+    header: "Experience",
+    description: "Experience OpenMetadata with demo data in seconds",
+    linkText: "Try Live Sandbox",
+    href: "https://sandbox.open-metadata.org/",
+  },
+  {
+    icon: `${ICON_ROUTE}/free-saas.svg`,
+    header: "Free SaaS",
+    description: "Free OpenMetadata cloud service with your own data",
+    linkText: "Sign Up Free",
+    href: "https://cloud.getcollate.io/signup",
+  }
+];
