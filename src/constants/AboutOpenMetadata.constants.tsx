ce1cc3c06775460e0de9bc66fcff8579e8bd85d2
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2024-06-26 11:28:13 +0530

    Add About openmetadata component (#137)
    * remove assets
    
    * add components for icons
    
    * update active tab colors
    
    * update styling for mobile view
    
    * update the icons
    
    * add integrations component
    
    * add try openmetadata component
    
    * add new tag in card
    
    * update new tag
    
    * add blogs component
    
    * update heading text size
    
    ---------
    
    Co-authored-by: Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>

---
+++ src/constants/AboutOpenMetadata.constants.tsx
@@ -0,0 +1,28 @@
+const IMAGE_ROUTE = '/assets/aboutOpenMetadata'
+
+export const ABOUT_OPENMETADATA = [
+    {
+        image: `${IMAGE_ROUTE}/vision.webp`,
+        header: 'Vision',
+        description: 'Announcing OpenMetadata',
+        href: 'https://blog.open-metadata.org/announcing-openmetadata-20399b816e60'
+    },
+    {
+        image: `${IMAGE_ROUTE}/product-news.webp`,
+        header: 'Product News',
+        description: 'OpenMetadata Release 1.4.0 - Lineage, Bulk Upload',
+        href: 'https://blog.open-metadata.org/openmetadata-release-1-4-0-f6fb11ec34d7'
+    },
+    {
+        image: `${IMAGE_ROUTE}/blog.webp`,
+        header: 'Blog',
+        description: 'Leveraging OpenMetadata’s Data Quality Framework',
+        href: 'https://medium.com/openmetadata/leveraging-the-power-of-openmetadata-data-quality-framework-385ba2d8eaf'
+    },
+    {
+        image: `${IMAGE_ROUTE}/video.webp`,
+        header: 'Video',
+        description: 'May Community Meeting',
+        href: 'https://medium.com/@arweave/community-spotlight-meeting-may-ff49c13db2e7'
+    },
+]
