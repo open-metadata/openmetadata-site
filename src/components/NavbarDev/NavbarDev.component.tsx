79ffb336f60b0e952bd337cc8ad5aa1de0716462
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-07-04 12:53:20 +0530

    update mobile design for navbar

--- src/components/NavbarDev/NavbarDev.component.tsx
+++ src/components/NavbarDev/NavbarDev.component.tsx
@@ -29,7 +29,7 @@ const NavbarDev = ({ onClick }: { onClick: () => void }) => {
       }`}
     >
       <div className="mx-auto custom-container flex justify-between items-center lg:px-5 lg:gap-3 max-lg:contents xl:px-[24px]">
-        <div className="flex items-center justify-between px-4 py-[12px] lg:px-0 lg:py-[10px]">
+        <div className="flex items-center justify-between px-[30px] py-[14px] lg:px-0 lg:py-[10px]">
           <Link className="text-primary" href="/">
             <Image
               className="w-[142px] h-[49px]"
--- src/components/NavbarDev/NavbarStrip.component.tsx
+++ src/components/NavbarDev/NavbarStrip.component.tsx
@@ -3,23 +3,23 @@ import Link from "next/link";
 
 const NavbarStrip = () => {
   return (
-    <div className="bg-[#0B3547] text-center mx-auto text-[#D3ECF7] py-2 lg:px-12">
-      <div className="custom-container flex justify-center items-center sm:gap-2 md:gap-3">
+    <div className="bg-[#0B3547] mx-auto text-[#D3ECF7] py-3 px-[32px] sm:px-[24px] md:py-2 lg:text-center lg:px-12">
+      <div className="custom-container flex justify-center items-center gap-3">
         <Image
-          className="md:inline"
+          className="w-9 h-9 sm:w-6 sm:h-6 md:inline"
           height={24}
           width={24}
           src="/assets/collate.svg"
           alt="collate-logo-svg"
           loading="eager"
         />
-        <div className="flex gap-3 items-center">
-          <span className="font-normal my-2 text-[16px] sm:my-0 lg:leading-[32px]">
+        <div className="max-w-[80%] lg:flex lg:gap-3 lg:items-center">
+          <span className="font-normal my-2 text-[15px] sm:my-0 lg:leading-[32px]">
             Try OpenMetadata as a managed service for free, from Collate.
           </span>
           <span className="lg:relative">
             <Link
-              className="text-[16px] border-none px-4 py-2 leading-[31.2px] underline decoration-1 underline-offset-[2px] lg:no-underline lg:border lg:border-[#D3ECF7] hover:text-[#D3ECF7]"
+              className="border-none lg:leading-[31.2px] underline decoration-1 underline-offset-[2px] pl-2 text-[15px] lg:py-2 lg:px-4 lg:no-underline lg:border lg:border-[#D3ECF7] hover:text-[#D3ECF7]"
               href="https://cloud.getcollate.io/signup"
               target="_blank"
             >
