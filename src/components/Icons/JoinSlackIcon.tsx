77bbafe2cc4caf0bf97994ef088735d6921d2636
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2024-07-05 15:23:41 +0530

    updated svgs into components (#156)

---
+++ src/components/Icons/JoinSlackIcon.tsx
@@ -0,0 +1,16 @@
+export const JoinSlackIcon = () => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.5 0H2.5C1.4 0 0.51 0.9 0.51 2L0.5 20L4.5 16H18.5C19.6 16 20.5 15.1 20.5 14V2C20.5 0.9 19.6 0 18.5 0ZM18.5 14H3.67L3.08 14.59L2.5 15.17V2H18.5V14ZM4.5 10H6.5V12H4.5V10ZM4.5 7H6.5V9H4.5V7ZM4.5 4H6.5V6H4.5V4ZM8.5 10H13.5V12H8.5V10ZM8.5 7H16.5V9H8.5V7ZM8.5 4H16.5V6H8.5V4Z"
+        fill="#7147E8"
+      />
+    </svg>
+  );
+};
