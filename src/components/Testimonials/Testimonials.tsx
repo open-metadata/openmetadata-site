d8115fef9484bf8eeaffc4088f08601c978ca7f6
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-07-24 17:58:52 +0530

    remove two column and add more and less buttons

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -32,6 +32,10 @@ const NextArrow = ({ onClick }: CustomArrowProps) => {
 
 const Testimonials = () => {
   const [activeSlide, setActiveSlide] = useState(0);
+  const [expandedDescription, setExpandedDescription] = useState(-1)
+
+  const handleMoreBtnClick = (index: number) => 
+    setExpandedDescription(expandedDescription === index ? -1 : index)
 
   const settings = {
     arrows: true,
@@ -68,85 +72,61 @@ const Testimonials = () => {
 
   return (
     <div className="custom-container relative z-[12]">
-      <div className="flex justify-between">
-        <Image
-          className="side-block"
-          height={20}
-          width={140}
-          src="/assets/header/left-box.svg"
-          alt="left-box-svg"
-          loading="eager"
-        />
-        <Image
-          className="side-block-big -translate-x-1/3"
-          height={20}
-          width={270}
-          src="/assets/header/left-box-big.svg"
-          alt="left-box-svg"
-          loading="eager"
-        />
-        <div className="mt-9 mx-auto px-0 md:px-16 md:mt-28 lg:px-0">
-          <h3 className="text-[#292929] tracking-[-0.02em] font-medium text-center max-w-[75%] mx-auto text-[32px] leading-[40px] mb-16 lg:text-[48px]">
-            Trusted <span className="text-[#7147E8]">Across Industries</span>
-          </h3>
-          <div className="slider-container testimonial-content">
-            <Slider {...settings}>
-              {TESTIMONIALS_LIST.map((item) => (
-                <div key={item.company}>
-                  <p className="text-[#170E2E] sm:columns-2 sm:gap-x-4 lg:gap-x-8 lg:text-[20px]">
-                    "{item.description}"
-                  </p>
-                  <div
-                    className={`mt-5 mx-auto flex items-center justify-center lg:max-w-[600px] ${
-                      (item.company === "beeline" || item.company === "Moove") && "mb-6"
-                    }`}
+      <div className="mt-9 mx-auto px-0 md:px-16 md:mt-28 lg:px-0">
+        <h3 className="text-[#292929] tracking-[-0.02em] font-medium text-center max-w-[75%] mx-auto text-[32px] leading-[40px] mb-16 lg:text-[48px]">
+          Trusted <span className="text-[#7147E8]">Across Industries</span>
+        </h3>
+        <div className="slider-container relative z-[4] testimonial-content">
+          <Slider {...settings}>
+            {TESTIMONIALS_LIST.map((item, i) => (
+              <div key={item.company}>
+                <span className="text-[#170E2E] text-justify lg:text-[20px]">
+                  "{expandedDescription !== i && item.description.length > 230 ? (
+                    item.description.slice(0, 230) + "..."
+                  ) : item.description}"{"  "}
+                </span>
+                {item.description.length > 230 && (
+                  <button 
+                    onClick={() => handleMoreBtnClick(i)} 
+                    className="relative z-[7] text-[#7147E8] cursor-pointer lg:text-[20px] hover:underline"
                   >
-                    <Link
-                      href={item.url}
-                      target="_blank"
-                      className="w-[250px] relative flex items-center"
-                    >
-                      <Image
-                        className={`text-right absolute right-0`}
-                        width={item.imgSize.width}
-                        height={item.imgSize.height}
-                        src={item.image}
-                        alt={item.company}
-                        priority
-                      />
-                    </Link>
-                    <div className="bg-[#E2DAFA] w-[5px] rounded-full h-[80px] mx-3 sm:mx-4"></div>
-                    <div className="w-[250px] self-center">
-                      <p className="text-[#382374] tracking-[-0.02em] text-[16px] font-medium">
-                        {item.name}
-                      </p>
-                      <p className="text-[#888888] tracking-[-0.02em] text-[14px] xl:-mt-4">
-                        {item.title}, {item.company}
-                      </p>
-                    </div>
+                    {expandedDescription === i ? "Less" : "More"}
+                  </button>
+                )}
+                <div
+                  className={`mt-5 mx-auto flex items-center justify-center lg:max-w-[600px] ${
+                    (item.company === "beeline" || item.company === "Moove") && "mb-6"
+                  }`}
+                >
+                  <Link
+                    href={item.url}
+                    target="_blank"
+                    className="w-[250px] relative flex items-center"
+                  >
+                    <Image
+                      className={`text-right absolute right-0`}
+                      width={item.imgSize.width}
+                      height={item.imgSize.height}
+                      src={item.image}
+                      alt={item.company}
+                      priority
+                    />
+                  </Link>
+                  <div className="bg-[#E2DAFA] w-[5px] rounded-full h-[80px] mx-3 sm:mx-4"></div>
+                  <div className="w-[250px] self-center">
+                    <p className="text-[#382374] tracking-[-0.02em] text-[16px] font-medium">
+                      {item.name}
+                    </p>
+                    <p className="text-[#888888] tracking-[-0.02em] text-[14px] xl:-mt-4">
+                      {item.title}, {item.company}
+                    </p>
                   </div>
                 </div>
-              ))}
-            </Slider>
-          </div>
+              </div>
+            ))}
+          </Slider>
         </div>
-        <Image
-          className="side-block"
-          height={20}
-          width={140}
-          src="/assets/header/right-box.svg"
-          alt="right-box-svg"
-          loading="eager"
-        />
-        <Image
-          className="side-block-big translate-x-1/3"
-          height={20}
-          width={270}
-          src="/assets/header/right-box-big.svg"
-          alt="right-box-svg"
-          loading="eager"
-        />
-      </div>
+      </div>  
     </div>
   );
 };
