25b4573ea392ca339f33f03ad1367320f2ef0570
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-08-01 11:20:38 +0530

    add clients logo for header

--- src/constants/Clients.constants.tsx
+++ src/constants/Clients.constants.tsx
@@ -10,6 +10,11 @@ export const CLIENT_LIST = [
         logo: `${LOGO_ROUTE}/intelligaia.svg`,
     },
     {
+        name: "Savii",
+        logo: `${LOGO_ROUTE}/savii.webp`,
+        width: 80
+    },
+    {
         name: "Beeline",
         logo: `${LOGO_ROUTE}/beeline.svg`,
         width: 40
@@ -19,8 +24,18 @@ export const CLIENT_LIST = [
         logo: `${LOGO_ROUTE}/khatabook.svg`,
     },
     {
+        name: "ZenBusiness",
+        logo:  `${LOGO_ROUTE}/zen-business.webp`,
+        width: 50
+    },
+    {
         name: "Lahaus",
         logo: `${LOGO_ROUTE}/lahaus.svg`,
+    },
+    {
+        name: "PayU Finance",
+        logo: `${LOGO_ROUTE}/payu-finance.webp`,
+        width: 100
     },
     {
         name: "Bucketplace",
@@ -39,6 +54,11 @@ export const CLIENT_LIST = [
         name: "Woop",
         logo: `${LOGO_ROUTE}/woop.webp`,
         width: 80
+    },
+    {
+        name: "Physics Wallah",
+        logo: `${LOGO_ROUTE}/physics-wallah.webp`,
+        width: 50
     },
     {
         name: "Hoodie Analytics",
