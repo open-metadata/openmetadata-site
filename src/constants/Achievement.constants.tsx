f011ce899a4c30d97646c476f6762d6f9c09db9a
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2024-08-29 20:30:32 +0530

    Add achievements (#175)
    * add achievements
    
    * updated icons

--- src/constants/Achievement.constants.tsx
+++ src/constants/Achievement.constants.tsx
@@ -3,22 +3,32 @@ const ICON_ROUTE = '/assets/achievement'
 export const ACHIEVEMENT_LIST = [
     {
         icon: `${ICON_ROUTE}/deployment.svg`,
-        count: '1,500',
+        count: '1,500+',
         name: 'Enterprise Deployments'
     },
     {
         icon: `${ICON_ROUTE}/open-source-members.svg`,
-        count: '6,000',
+        count: '6,000+',
         name: 'Open Source Members'
     },
     {
         icon: `${ICON_ROUTE}/code-contributors.svg`,
-        count: '250',
+        count: '250+',
         name: 'Code Contributors'
     },
     {
         icon: `${ICON_ROUTE}/github-star.svg`,
-        count: '5,000',
+        count: '5,000+',
         name: 'GitHub Stars'
+    },
+    {
+        icon: `${ICON_ROUTE}/data-assets.svg`,
+        count: '2+ Million',
+        name: 'Data Assets At Large Deployments'
+    },
+    {
+        icon: `${ICON_ROUTE}/data-connector.svg`,
+        count: '80+',
+        name: 'Data Connectors'
     },
 ]
