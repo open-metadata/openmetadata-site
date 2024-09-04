5b07d54a67034067f7d5d65ed3c1c5b4e58fc80e
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-09-04 13:27:49 +0530

    update website content for release 1.5.0 (#170)
    * update website content for release 1.5.0
    
    * Update AboutOpenMetadata.constants.tsx for OpenMetadata Release 1.5.0

--- src/components/IntegrationsDev/IntegrationsDev.tsx
+++ src/components/IntegrationsDev/IntegrationsDev.tsx
@@ -1,7 +1,7 @@
-import { INTEGRATION_SERVICES } from "@/constants/Integrations.constants";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { INTEGRATION_SERVICES } from '@/constants/Integrations.constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface Service {
   href?: string;
@@ -17,7 +17,7 @@ const IntegrationsDev = () => {
     .flat()
     .sort((a, b) => a.name.localeCompare(b.name));
 
-  const [selectedTab, setSelectedTab] = useState<string>("All Connectors");
+  const [selectedTab, setSelectedTab] = useState<string>('All Connectors');
   const [services, setServices] = useState<Service[]>(allServices);
   const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
   const [mobileServices, setMobileServices] = useState<Service[]>(
@@ -28,7 +28,7 @@ const IntegrationsDev = () => {
     setSelectedTab(tab);
     setIsDropdownVisible(false);
     let filteredServices: Service[];
-    if (tab && tab !== "All Connectors") {
+    if (tab && tab !== 'All Connectors') {
       const selectedConnector = INTEGRATION_SERVICES.find(
         (connector) => connector.connector === tab
       );
@@ -62,7 +62,7 @@ const IntegrationsDev = () => {
         OpenMetadata's ingestion framework supports connectors for 80+ data services, 
         with more added every release.
       </p>
-      <div className="mt-[48px] overflow-y-scroll relative shadow-primary rounded-lg md:rounded-2xl sm:flex sm:h-[728px] md:h-[780px]">
+      <div className="mt-[48px] overflow-y-scroll relative shadow-primary rounded-lg md:rounded-2xl sm:flex sm:h-[808px] md:h-[866px]">
         <button
           onClick={() => setIsDropdownVisible((prev) => !prev)}
           className="py-[32px] w-full sticky top-0 bg-white z-[15] flex items-center border-b border-[#E3DAFA] justify-center gap-2 text-[#5E3BC1] text-[20px] font-normal tracking-[-0.02em] sm:hidden"
@@ -70,7 +70,7 @@ const IntegrationsDev = () => {
           <span>{selectedTab}</span>
           <svg
             className={`${
-              isDropdownVisible ? "rotate-180" : ""
+              isDropdownVisible ? 'rotate-180' : ''
             } transition-all duration-300`}
             width="19"
             height="12"
@@ -89,15 +89,15 @@ const IntegrationsDev = () => {
         </button>
         <div
           className={`${
-            isDropdownVisible ? "block" : "hidden"
+            isDropdownVisible ? 'block' : 'hidden'
           } integration w-full absolute flex flex-col bg-white text-[#5E3BC1] text-[20px] text-center sm:w-auto sm:block sm:relative sm:text-[16px] sm:border-[#E3DAFA] sm:border-r md:text-[20px] sm:rounded-l-lg md:rounded-l-2xl lg:max-w-[20%] xl:max-w-[25%]`}
         >
           <div
             className={`px-5 py-[28px] cursor-pointer tracking-[-0.02em] text-nowrap ${
-              selectedTab === "All Connectors" &&
-              "font-semibold bg-[#EDF3FD] sm:rounded-tl-lg md:rounded-tl-2xl"
+              selectedTab === 'All Connectors' &&
+              'font-semibold bg-[#EDF3FD] sm:rounded-tl-lg md:rounded-tl-2xl'
             } border-b border-[#E3DAFA] lg:px-9 xl:px-12`}
-            onClick={() => handleTabClick("All Connectors")}
+            onClick={() => handleTabClick('All Connectors')}
           >
             All Connectors
           </div>
@@ -106,13 +106,13 @@ const IntegrationsDev = () => {
               key={service.connector}
               className={`px-5 py-[28px] tracking-[-0.02em] cursor-pointer ${
                 selectedTab === service.connector &&
-                "font-semibold bg-[#EDF3FD]"
+                'font-semibold bg-[#EDF3FD]'
               } ${
-                selectedTab === "Others" &&
-                service.connector === "Others" &&
-                "sm:rounded-tr-lg md:rounded-bl-2xl"
+                selectedTab === 'Others' &&
+                service.connector === 'Others' &&
+                'sm:rounded-tr-lg md:rounded-bl-2xl'
               } ${
-                service.connector === "Others" && "sm:border-none"
+                service.connector === 'Others' && 'sm:border-none'
               } border-b border-[#E3DAFA] lg:px-9 xl:px-12`}
               onClick={() => handleTabClick(service.connector)}
             >
@@ -124,7 +124,7 @@ const IntegrationsDev = () => {
           {services.map((item) => (
             <div key={item.name} className="max-h-[110px]">
               <Link
-                href={item.href ?? "#"}
+                href={item.href ?? '#'}
                 target="_blank"
                 className="text-center flex flex-col items-center"
               >
@@ -132,7 +132,7 @@ const IntegrationsDev = () => {
                   alt={item.alt}
                   width={1000}
                   className={`h-12 mb-2 w-12 ${
-                    item.name === "SAP HANA" && "w-20"
+                    item.name === 'SAP HANA' && 'w-20'
                   }`}
                   height={1000}
                   src={item.src}
@@ -150,7 +150,7 @@ const IntegrationsDev = () => {
             {mobileServices.map((item) => (
               <div key={item.name} className="max-h-[110px]">
                 <Link
-                  href={item.href ?? "#"}
+                  href={item.href ?? '#'}
                   target="_blank"
                   className="text-center flex flex-col items-center"
                 >
@@ -158,7 +158,7 @@ const IntegrationsDev = () => {
                     alt={item.alt}
                     width={1000}
                     className={`h-12 mb-2 w-12 ${
-                      item.name === "SAP HANA" && "w-20"
+                      item.name === 'SAP HANA' && 'w-20'
                     }`}
                     height={1000}
                     src={item.src}
