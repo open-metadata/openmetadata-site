86015f08b369e4c1b7c7cb3d56704ad2fbd245fa
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-09-18 13:51:13 +0530

    minor: update deployment links (#182)

--- src/constants/TryOpenMetadata.constants.tsx
+++ src/constants/TryOpenMetadata.constants.tsx
@@ -1,28 +1,28 @@
-const ICON_ROUTE = "/assets/tryOpenMetadata";
+const ICON_ROUTE = '/assets/tryOpenMetadata';
 
 export const TRY_OPENMETADATA = [
   {
     icon: `${ICON_ROUTE}/deploy-card.svg`,
-    header: "Deploy",
-    description: "Deploy OpenMetadata in your own environment",
-    linkText: "Download Now",
-    href: "https://docs.open-metadata.org/deployment",
-    isNew: false
+    header: 'Deploy',
+    description: 'Deploy OpenMetadata in your own environment',
+    linkText: 'Download Now',
+    href: 'https://docs.open-metadata.org/latest/quick-start/local-docker-deployment',
+    isNew: false,
   },
   {
     icon: `${ICON_ROUTE}/experience-card.svg`,
-    header: "Experience",
-    description: "Experience OpenMetadata with demo data in seconds",
-    linkText: "Try Live Sandbox",
-    href: "https://sandbox.open-metadata.org/",
-    isNew: false
+    header: 'Experience',
+    description: 'Experience OpenMetadata with demo data in seconds',
+    linkText: 'Try Live Sandbox',
+    href: 'https://sandbox.open-metadata.org/',
+    isNew: false,
   },
   {
     icon: `${ICON_ROUTE}/free-saas-card.svg`,
-    header: "Free SaaS",
-    description: "Free OpenMetadata cloud service with your own data",
-    linkText: "Sign Up Free",
-    href: "https://cloud.getcollate.io/signup",
-    isNew: true
-  }
+    header: 'Free SaaS',
+    description: 'Free OpenMetadata cloud service with your own data',
+    linkText: 'Sign Up Free',
+    href: 'https://cloud.getcollate.io/signup',
+    isNew: true,
+  },
 ];
