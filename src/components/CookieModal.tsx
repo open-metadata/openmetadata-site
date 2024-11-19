33f4033266bedb366557474ee5ac64a7e6acd4de
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-11-19 12:09:35 +0530

    update as per comments

--- src/components/CookieModal.tsx
+++ src/components/CookieModal.tsx
@@ -13,38 +13,34 @@ const CookieModal = () => {
         const userCookie = localStorage.getItem('userCookie');
         setCookieValue(userCookie);
         setLoading(false);
-    }, []);
+    }, [cookieValue]);
 
-    if (loading) return null;
+    if (loading || cookieValue) return null;
 
     return (
-        <>
-            {!cookieValue && (
-                <div className="fixed bottom-0 bg-[#F8FBFC] z-[20] p-6 shadow-shadow card sm:m-5 sm:max-w-xl lg:max-w-3xl">
-                    <p className="text-[#292929] font-light">
-                        By clicking &quot;Accept&quot;, you agree to the storing of
-                        cookies on your device to enhance site navigation,
-                        improve your experience, and assist in our marketing
-                        efforts. To learn more, read our Privacy Policy.
-                    </p>
+        <div className="fixed bottom-0 bg-[#F8FBFC] z-[20] p-6 shadow-shadow card sm:m-5 sm:max-w-xl lg:max-w-3xl">
+            <p className="text-[#292929] font-light">
+                By clicking &quot;Accept&quot;, you agree to the storing of
+                cookies on your device to enhance site navigation,
+                improve your experience, and assist in our marketing
+                efforts. To learn more, read our Privacy Policy.
+            </p>
 
-                    <div className="mt-8 flex gap-3 justify-end">
-                        <button
-                            className="text-white bg-[#7147E8] rounded-3xl py-2 px-8 text-sm"
-                            onClick={() => handleButtonClick('Accept')}
-                        >
-                            Accept
-                        </button>
-                        <button
-                            className="text-[#7147E8] bg-white border border-[#7147E8] rounded-3xl py-2 px-8 text-sm"
-                            onClick={() => handleButtonClick('Decline')}
-                        >
-                            Decline
-                        </button>
-                    </div>
-                </div>
-            )}
-        </>
+            <div className="mt-8 flex gap-3 justify-end">
+                <button
+                    className="text-white bg-[#7147E8] rounded-3xl py-2 px-8 text-sm"
+                    onClick={() => handleButtonClick('Accept')}
+                >
+                    Accept
+                </button>
+                <button
+                    className="text-[#7147E8] bg-white border border-[#7147E8] rounded-3xl py-2 px-8 text-sm"
+                    onClick={() => handleButtonClick('Decline')}
+                >
+                    Decline
+                </button>
+            </div>
+        </div>        
     )
 }
 
