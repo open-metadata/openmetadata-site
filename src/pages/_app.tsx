aeb1229e7af22b99b2341e9a50b46410434743ad
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2024-12-19 11:04:54 +0530

    Add cookie banner (#193)
    * add cookie banner
    
    * update cookie modal for all pages
    
    * update as per comments
    
    * update cookie modal functionality and transition
    
    * update cookie modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/styles.css';
 import '../styles/globals.css';
 import '../styles/cve-style.css';
+import '../styles/modal.css';
 import type { AppProps } from 'next/app';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import '@fontsource/metropolis';
@@ -11,13 +12,42 @@ import '@fontsource/metropolis/700.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Script from 'next/script';
 import Head from 'next/head';
+import CookieModal from '@/components/CookieModal';
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [storedCookie, setStoredCookie] = useState<string | null>(null);
+
+  const handleButtonClick = (choice: string) => {
+    localStorage.setItem("userCookie", choice);
+    setStoredCookie(choice); 
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const userCookie = window.localStorage.getItem("userCookie");
+      setStoredCookie(userCookie);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (storedCookie === "Decline") {
+      const scriptTags = document.querySelectorAll(
+        'script[src*="googletagmanager"], script#gtag-init, script#tag-manager'
+      );
+      scriptTags.forEach((tag) => tag.remove());
+
+      const iframes = document.querySelectorAll(
+        'iframe[src*="googletagmanager"]'
+      );
+      iframes.forEach((iframe) => iframe.remove());
+    }
+  }, [storedCookie]);
+
   useEffect(() => {
     AOS.init({
       offset: 80,
@@ -27,7 +57,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      {/* Google Tag Manager */}
+      {(!storedCookie || storedCookie === "Accept") && (
+        <>
+          {/* Google Tag Manager */}
       <Head>
         {/* eslint-disable-next-line @next/next/next-script-for-ga */}
         <script
@@ -74,6 +106,9 @@ export default function App({ Component, pageProps }: AppProps) {
         id="gtag-init"
         strategy="afterInteractive"
       />
+        </>
+      )}
+      {!storedCookie && <CookieModal handleButtonClick={handleButtonClick} />}
       <Component {...pageProps} />
     </>
   );
