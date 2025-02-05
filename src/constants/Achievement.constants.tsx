d2e47636e291412b21276690caa89925f1f903f8
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2025-02-04 18:47:53 -0800

    Update Achievement.constants.tsx (#205)

--- src/constants/Achievement.constants.tsx
+++ src/constants/Achievement.constants.tsx
@@ -3,22 +3,22 @@ const ICON_ROUTE = '/assets/achievement'
 export const ACHIEVEMENT_LIST = [
     {
         icon: `${ICON_ROUTE}/deployment.svg`,
-        count: '1,500+',
+        count: '2,000+',
         name: 'Enterprise Deployments'
     },
     {
         icon: `${ICON_ROUTE}/open-source-members.svg`,
-        count: '7,800+',
+        count: '8,000+',
         name: 'Open Source Members'
     },
     {
         icon: `${ICON_ROUTE}/code-contributors.svg`,
-        count: '290+',
+        count: '300+',
         name: 'Code Contributors'
     },
     {
         icon: `${ICON_ROUTE}/github-star.svg`,
-        count: '5,800+',
+        count: '6,000+',
         name: 'GitHub Stars'
     },
     {
@@ -28,7 +28,7 @@ export const ACHIEVEMENT_LIST = [
     },
     {
         icon: `${ICON_ROUTE}/data-connector.svg`,
-        count: '80+',
+        count: '90+',
         name: 'Data Connectors'
     },
 ]
