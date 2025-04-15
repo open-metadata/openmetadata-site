42848d73983de5a7cb29f35955fdcaa1e503a24d
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-04-15 15:53:31 +0530

    update tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -59,34 +59,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       {(!storedCookie || storedCookie === "Accept") && (
         <>
-          {/* Google Tag Manager */}
-      <Head>
-        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
-        <script
-          defer
-          id="tag-manager"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-554C968W');
-            `,
-          }}
-        />
-      </Head>
-
-      {/* Tag manager no script */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-554C968W"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
-
       {/* Tag manager no script */}
       <Script
         src={'https://www.googletagmanager.com/gtag/js?id=G-LBLK284YQW'}
