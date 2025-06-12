0d9c9131ffa2a2d600e7ffd661cd03f853a1db38
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-06-12 22:10:20 +0530

    update canonical

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,20 @@
-import '../styles/styles.css';
-import '../styles/globals.css';
-import '../styles/cve-style.css';
-import '../styles/modal.css';
-import type { AppProps } from 'next/app';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import '@fontsource/metropolis';
-import '@fontsource/metropolis/400-italic.css';
-import '@fontsource/metropolis/500.css';
-import '@fontsource/metropolis/600.css';
-import '@fontsource/metropolis/700.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect, useState } from 'react';
-import Head from 'next/head';
-import CookieModal from '@/components/CookieModal';
+import "../styles/styles.css";
+import "../styles/globals.css";
+import "../styles/cve-style.css";
+import "../styles/modal.css";
+import type { AppProps } from "next/app";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "@fontsource/metropolis";
+import "@fontsource/metropolis/400-italic.css";
+import "@fontsource/metropolis/500.css";
+import "@fontsource/metropolis/600.css";
+import "@fontsource/metropolis/700.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import CookieModal from "@/components/CookieModal";
 
 config.autoAddCss = false;
 
@@ -23,7 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   const handleButtonClick = (choice: string) => {
     localStorage.setItem("userCookie", choice);
-    setStoredCookie(choice); 
+    setStoredCookie(choice);
   };
 
   useEffect(() => {
@@ -59,74 +59,77 @@ export default function App({ Component, pageProps }: AppProps) {
       {(!storedCookie || storedCookie === "Accept") && (
         <>
           {/* Google Tag Manager */}
-      <Head>
-      <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="canonical" href={`https://open-metadata.org${pageProps.link}`} />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta
-          name="description"
-          content="OpenMetadata is the #1 open source data catalog tool with the all-in-one platform for data discovery, quality, governance, collaboration &amp; more. Join our community to stay updated."
-        />
-        <meta
-          name="keywords"
-          content="best open-source data catalog community, #1 open source data catalog, enterprise data catalog, data governance solutions, centralized metadata platform, best data discovery tool, data collaboration platform, modern data catalog, data catalog data lineage, best metadata management tool"
-        />
-        <meta name="author" content="open-metadata" />
-        <meta name="publisher" content="Collate" />
+          <Head>
+            <meta charSet="utf-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <link
+              rel="canonical"
+              href={`https://open-metadata.org${pageProps.link}`}
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            <meta
+              name="description"
+              content="OpenMetadata is the #1 open source data catalog tool with the all-in-one platform for data discovery, quality, governance, collaboration &amp; more. Join our community to stay updated."
+            />
+            <meta
+              name="keywords"
+              content="best open-source data catalog community, #1 open source data catalog, enterprise data catalog, data governance solutions, centralized metadata platform, best data discovery tool, data collaboration platform, modern data catalog, data catalog data lineage, best metadata management tool"
+            />
+            <meta name="author" content="open-metadata" />
+            <meta name="publisher" content="Collate" />
 
-        {/* Open Graph tags start  */}
-        <meta
-          property="og:title"
-          content="OpenMetadata: Join the #1 Open Source Data Community"
-        />
-        <meta
-          property="og:description"
-          content="OpenMetadata is the #1 open source data catalog tool with the all-in-one platform for data discovery, quality, governance, collaboration &amp; more. Join our community to stay updated."
-        />
-        <meta
-          property="og:image"
-          content="https://open-metadata.org/assets/favicon.png"
-        />
-        <meta property="og:type" content="metadata tool" />
-        {/* Open Graph tags end */}
+            {/* Open Graph tags start  */}
+            <meta
+              property="og:title"
+              content="OpenMetadata: Join the #1 Open Source Data Community"
+            />
+            <meta
+              property="og:description"
+              content="OpenMetadata is the #1 open source data catalog tool with the all-in-one platform for data discovery, quality, governance, collaboration &amp; more. Join our community to stay updated."
+            />
+            <meta
+              property="og:image"
+              content="https://open-metadata.org/assets/favicon.png"
+            />
+            <meta property="og:type" content="metadata tool" />
+            {/* Open Graph tags end */}
 
-        {/* Twitter Card start */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="https://open-metadata.org" />
-        <meta
-          name="twitter:title"
-          content="OpenMetadata: Join the #1 Open Source Data Community"
-        />
-        <meta
-          name="twitter:description"
-          content="OpenMetadata is the #1 open source data catalog tool with the all-in-one platform for data discovery, quality, governance, collaboration &amp; more. Join our community to stay updated."
-        />
-        <meta
-          name="twitter:image"
-          content="https://open-metadata.org/assets/favicon.png"
-        />
-        {/* Twitter Card end */}
+            {/* Twitter Card start */}
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="https://open-metadata.org" />
+            <meta
+              name="twitter:title"
+              content="OpenMetadata: Join the #1 Open Source Data Community"
+            />
+            <meta
+              name="twitter:description"
+              content="OpenMetadata is the #1 open source data catalog tool with the all-in-one platform for data discovery, quality, governance, collaboration &amp; more. Join our community to stay updated."
+            />
+            <meta
+              name="twitter:image"
+              content="https://open-metadata.org/assets/favicon.png"
+            />
+            {/* Twitter Card end */}
 
-        <title>OpenMetadata: Join the #1 Open Source Data Community</title>
-        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
-        <script
-          defer
-          id="tag-manager"
-          dangerouslySetInnerHTML={{
-            __html: `
+            <title>OpenMetadata: Join the #1 Open Source Data Community</title>
+            {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+            <script
+              defer
+              id="tag-manager"
+              dangerouslySetInnerHTML={{
+                __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','GTM-554C968W');
             `,
-          }}
-        />
-      </Head>
+              }}
+            />
+          </Head>
         </>
       )}
       {!storedCookie && <CookieModal handleButtonClick={handleButtonClick} />}
