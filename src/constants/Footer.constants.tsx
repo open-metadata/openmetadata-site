752c029c1e15484b9fd41ac14942f897e7a3d193
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-06-13 16:23:55 +0530

    update sizs

--- src/constants/Footer.constants.tsx
+++ src/constants/Footer.constants.tsx
@@ -3,19 +3,16 @@ export const SOCIAL_LINKS = [
         name: "Slack",
         url: "https://slack.open-metadata.org/",
         icon: "/assets/logo/slack.svg",
-        className: "w-[28px] h-[30px]",
     },
     {
         name: "GitHub",
         url: "https://github.com/open-metadata/openmetadata",
         icon: "/assets/logo/github.svg",
-        className: "w-[28px] h-[30px]",
     },
     {
         name: "X",
         url: "https://twitter.com/open_metadata",
         icon: "/assets/logo/x.svg",
-        className: "w-[28px] h-[30px]",
     },
     {
         name: "Meetup",
