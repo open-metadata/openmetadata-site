9855a0df0390ef9192b75607f1810f4efef09fce
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-06-16 16:11:20 +0530

    update styles

--- src/pages/case-studies.tsx
+++ src/pages/case-studies.tsx
@@ -88,7 +88,7 @@ const CaseStudiesPage = () => {
               {HEADER_CUSTOMERS.map((customer, index) => (
                 <div
                   key={customer.title}
-                  className={`bg-white rounded-[10px] p-3 card-shadow ${
+                  className={`bg-white rounded-[10px] p-3 card-shadow flex flex-col h-full ${
                     index < 2 ? "sm:col-span-3" : "sm:col-span-3 lg:col-span-2"
                   }`}
                 >
@@ -117,8 +117,9 @@ const CaseStudiesPage = () => {
                   >
                     {customer.title}
                   </div>
+                  <div className="flex-1" />
                   <ParamLink href={customer.url}>
-                    <div className="mt-5 mb-2 rounded-[3px] font-medium tracking-[-0.02em] mx-2 border-2 border-[#7147E8] max-w-fit px-4 py-2 text-[#7147E8]">
+                    <div className="mt-5 mb-2 rounded-[3px] font-medium tracking-[-0.02em] mx-2 border-2 border-[#7147E8] max-w-fit px-4 py-2 text-[#7147E8] whitespace-nowrap">
                       {customer.buttonText}
                     </div>
                   </ParamLink>
