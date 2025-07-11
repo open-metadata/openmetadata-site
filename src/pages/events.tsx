04687f824d27d7851646a9439d4f6faa211f0e5e
Sriharsha Chintalapani <harsha@getcollate.io>
2025-07-11 01:30:38 -0700

    Add Events Page

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -38,19 +38,21 @@ export default function Events() {
                 Join our events to learn more about OpenMetadata
               </p>
             </div>
-            <div className="relative w-full overflow-hidden rounded-lg" style={{ height: 'calc(100vh - 300px)', minHeight: '600px' }}>
-              <iframe
-                src="https://lu.ma/embed/calendar/cal-vu0BP4BUofmA5oU/events"
-                className="absolute top-0 left-0 w-full h-full"
-                frameBorder="0"
-                style={{ 
-                  border: '1px solid #bfcbda88', 
-                  borderRadius: '4px'
-                }}
-                allowFullScreen
-                aria-hidden="false"
-                tabIndex={0}
-              />
+            <div className="max-w-4xl mx-auto">
+              <div className="relative w-full overflow-hidden rounded-lg" style={{ height: '800px' }}>
+                <iframe
+                  src="https://lu.ma/embed/calendar/cal-vu0BP4BUofmA5oU/events"
+                  className="absolute top-0 left-0 w-full h-full"
+                  frameBorder="0"
+                  style={{ 
+                    border: '1px solid #bfcbda88', 
+                    borderRadius: '4px'
+                  }}
+                  allowFullScreen
+                  aria-hidden="false"
+                  tabIndex={0}
+                />
+              </div>
             </div>
           </div>
         </section>
