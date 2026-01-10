ec26d33073841b14b1a15385ec18c4d6230ecca5
Nick Acosta <nick.acosta@getcollate.io>
2026-01-09 16:27:53 -0800

    vrt case study grammar clean up

--- src/constants/VRTCustomer.constants.tsx
+++ src/constants/VRTCustomer.constants.tsx
@@ -1,6 +1,6 @@
 export const VRT_CUSTOMER_HEADER = {
     title: "VRT automates their self-service data quality loop with OpenMetadata",
-    description: 'VRT is the public broadcaster of the Flemish Community in Belgium. With its three television channels, five radio stations and various digital channels, VRT reaches 90% of all Flemish people, every single week. Dataroots is partnering with VRT to maintain and expand a modern data platform with a focus on data governance and data quality. With a growing number of teams, data systems, and touch points, there is an increased importance on getting metadata correct and consistent while keeping manually intervention from Data Engineers to a minimum. By implementing OpenMetadata, VRT built a single point of truth that was easily configurable and scalable that could allow business users to address new data quality issues immediately without developer intervention.',
+    description: 'VRT is the public broadcaster of the Flemish Community in Belgium. With three television channels, five radio stations and various digital channels, VRT reaches 90% of all Flemish people, every single week. Dataroots is partnering with VRT to maintain and expand a modern data platform with a focus on data governance and data quality. The growing number of teams, data systems, and touch points has necessitated an increased importance in getting metadata correct and consistent while keeping manually intervention from Data Engineers to a minimum. By implementing OpenMetadata, VRT built a single point of truth that was easily configurable and scalable where business users can address new data quality issues immediately without developer intervention.',
     industry: "Other / Media",
     technologies: "Amazon Athena, Amazon S3, dbt, AWS Lambda",
     image: "/assets/vrt/vrt-header.png"
@@ -9,7 +9,7 @@ export const VRT_CUSTOMER_HEADER = {
 export const VRT_CUSTOMER_HIGHLIGHTS = [
     {
         id: 1,
-        count: '2.3 Million',
+        count: '2.3+ Million',
         description: "Daily TV Viewers"
     },
     {
@@ -19,8 +19,8 @@ export const VRT_CUSTOMER_HIGHLIGHTS = [
     },
     {
         id: 3,
-        count: '30',
-        description: "seconds to set up a custom data quality alert in OpenMetadata"
+        count: '30 seconds',
+        description: "to set up a custom data quality alert in OpenMetadata"
     }
 ];
 
@@ -40,11 +40,11 @@ To ensure data quality at VRT gets resolved as quickly as possible, Dataroots bu
         challenges: [
             {
                 header: 'Implementing data quality tests with non-technical teams',
-                description: 'To inspire ownership, Dataroots needs to make sure that the owners have the needed tools/information to fix issues.',
+                description: 'To inspire ownership, Dataroots needed to make sure data owners have the right tools/information to fix issues.',
             },
             {
                 header: 'Managing metadata across multiple touchpoints',
-                description: 'With a single CMS powering content for a streaming service, news site, and many other touchpoints, VRT needs consistent and accurate metadata to prevent errors, like content showing up in the wrong UI location, from disrupting the customer experience.', 
+                description: 'With a single CMS powering content for a streaming service, news site, and many other touchpoints, VRT needed consistent and accurate metadata to prevent errors, like content showing up in the wrong UI location, from disrupting the customer experience.', 
             },
 
        ]
@@ -55,29 +55,29 @@ To ensure data quality at VRT gets resolved as quickly as possible, Dataroots bu
         challenges: [
             {
                 header: 'Growing test cases',
-                description: 'Integrating test cases from dbt, OpenMetadata, and custom-built tests increases VRT coverage, but their initial alerts were being sent directly to Data Engineers, who would work with data owners to implement a fix.',
+                description: 'Integrating test cases from dbt, OpenMetadata, and custom-built tests increased VRT coverage, but their initial alerts were being sent directly to Data Engineers, who would work with data owners to implement a fix.',
             },
             {
                 header: 'Self-service prevented costly bottlenecks',
-                description: "Data Engineers quickly became bottlenecks in the resolution process as data tests grew, with frequent context switching between alerts and other work taking focus and preventing ownership of stakeholder team. By customizing alerts in OpenMetadata, Dataroots was able to create alerts that went directly to the single point of contant responsible for owning the data asset!",
+                description: "Data Engineers quickly became bottlenecks in the resolution process as data tests grew, with frequent context switching between alerts and other work taking away focus and preventing ownership of stakeholder team. By customizing alerts in OpenMetadata, Dataroots created alerts that went directly to the single point of contant responsible for owning the data asset!",
             },
         ]
     },
     {
         title: 'Developing scalable and customizable alerts without maintainence fatigue',
-        description: 'With OpenMetadata, Dataroots was able to provide the right alerts to the right stakeholders as soon as they appeared, but their ideal solution involved new issues getting fixed immediately. This pipeline freed up time that the VRT team could use for core tasks while giving owners responsibility, agency, and paths to resoultion in a shortened time with a reduced number of steps.',
+        description: 'With OpenMetadata, Dataroots provides the right alerts to the right stakeholders as soon as they appeared, but their ideal solution would go even further, with immediate fixes to new issues. This pipeline freed up time that the VRT team could use for core tasks while giving owners responsibility, agency, and paths to resoultion in a shortened time with a reduced number of steps.',
         challenges: [
             {
                 header: 'Reducing time and steps to resolution',
-                description: 'Through Lambda functions, Dataroots would parse alerts coming from OpenMetadata and recreate the incident in Athena and store results in an bucket that is mailed to the asset owner.',
+                description: 'Through Lambda functions, Dataroots now parses alerts coming from OpenMetadata and recreates the incident in Athena before emailing the results to the asset owner.',
             },
             {
                 header: '30 seconds to a custom alert',
-                description: 'Dataroots was able to scale this configuration for every data quality test in seconds with the  easy alerting configurations available in OpenMetadata.',
+                description: 'Dataroots has scaled there data quality alerting configuration for every data quality test, taking just seconds to add new tests with the easy alerting setup available in OpenMetadata.',
             },
             {
                 header: 'Dynamic, multifunctional mail with all relevant information',
-                description: 'Through OpenMetadata, Dataroots was able to prepare emails that not only included the data that was causing tests to fail, but also details and hyperlinks to the test in OpenMetadata, and every links directly to query engines so that VRT data teams could investigate further.',
+                description: 'Through OpenMetadata, emails now not only include data that is causing a test to fail, but also details and hyperlinks to the test in OpenMetadata, and even links directly to query engines so that VRT data teams can investigate further.',
             },
             {
                 header: 'Foundation for AI innovation',
