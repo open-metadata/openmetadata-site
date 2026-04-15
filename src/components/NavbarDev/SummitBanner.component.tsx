9bd25949544cd6712d0068a61f7ba31f0ffddb6f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2026-04-15 16:39:55 +0530

    updated colors

--- src/components/NavbarDev/SummitBanner.component.tsx
+++ src/components/NavbarDev/SummitBanner.component.tsx
@@ -2,8 +2,8 @@ import ParamLink from "../ParamLink";
 
 const SummitBanner = () => {
   return (
-    <div className="w-full py-1 px-5 bg-[#0B3547] text-center">
-      <span className="text-sm font-normal text-white">
+    <div className="w-full py-[6px] px-5 text-center" style={{ background: "linear-gradient(270deg, #ECE5FF 39.9%, #7147E8 114.41%)" }}>
+      <span className="text-black text-sm font-medium">
         OpenMetadata production stories from OpenAI, Yelp, Rakuten &amp; more —{" "}
         <ParamLink
           name="Register Now for Collate Summit '26"
