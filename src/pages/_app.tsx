3cae379c5e7350702423516474dab0f85c9b9935
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2026-06-15 14:59:25 +0530

    Fix vulnerability (#373)
    * Fix vulnerability
    
    * fix mdx remote vulnerability
    
    * fix postcss vulnerability
    
    * fix warnings
    
    * fix vulnerability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import Script from "next/script";
 import CookieModal from "@/components/CookieModal";
 import Layout from "@/components/Layout/Layout";
 
@@ -124,26 +125,22 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>
           {pageProps.title ?? "OpenMetadata: #1 Open Source Context Layer"}
         </title>
-        {(!storedCookie || storedCookie === "Accept") && (
-          <>
-            {/* Google Tag Manager */}
-            {/* eslint-disable-next-line @next/next/next-script-for-ga */}
-            <script
-              defer
-              id="tag-manager"
-              dangerouslySetInnerHTML={{
-                __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-554C968W');
-            `,
-              }}
-            />
-          </>
-        )}
       </Head>
+      {(!storedCookie || storedCookie === "Accept") && (
+        <Script
+          id="tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-554C968W');
+        `,
+          }}
+        />
+      )}
       <Layout>
         {!storedCookie && <CookieModal handleButtonClick={handleButtonClick} />}
         <Component {...pageProps} />
